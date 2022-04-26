a75c91bc601dc69606bbd1098d3a605f14225eb0
Marie-Demeter <mdemeter.ducrot@gmail.com>
2022-04-26 14:05:17 +0200

    feature :  création, suppresion GET POST DELETE

--- functions.js
+++ functions.js
@@ -1,12 +1,12 @@
 import fs from 'fs/promises';
 
-async function listFiles(path) {
+async function listDirectory2(path) {
     const dirents = await fs.readdir(path, { withFileTypes: true });
     const tabFiles = await Promise.all(dirents.map(dirent => transformDirent(dirent, path)));
     return tabFiles;
 }
 
-async function transformDirent(dirent, path) {
+async function transformDirent2(dirent, path) {
     const file = {
         name: dirent.name,
         isFolder: dirent.isDirectory(),
@@ -18,35 +18,67 @@ async function transformDirent(dirent, path) {
     return file;
 }
 
+function getStatsFile2(dirent, path) {
+    return fs.stat(path + dirent.name)
+}
+
+
+function listDirectory(path) {
+    return fs.readdir(path, { withFileTypes: true })
+        .then(dirents => dirents.map(dirent => transformDirent(dirent, path)));
+}
+
+function transformDirent(dirent, path) {
+    const file = {
+        name: dirent.name,
+        isFolder: dirent.isDirectory(),
+    }
+    if (dirent.isFile()) {
+        file.size = getStatsFile(dirent, path)
+            .then(statsFile=>statsFile.size);
+    }
+    return file;
+}
+
 function getStatsFile(dirent, path) {
     return fs.stat(path + dirent.name)
 }
-/*
-async function printFileOrFolder(req, res, path) {
-    
-    const fileName = req.params.name;
-    const regexFolderName = new RegExp ("^[a-zA-Z0-9_\-]+$","gm");
-    const isFolder = regexFolderName.test(fileName);
 
-    if(isFolder){
-        const tabFiles = await listFiles(path+fileName+"/").catch(err=>console.log(err.message));
-        return res.status(200).type('application/json').send(tabFiles);
+
+
+
+
+async function createDirectory(path, folderName){
+
+    const regexFolderName = new RegExp ("^[a-zA-Z]+$","gm");
+    const isAlphanum = regexFolderName.test(folderName);
+
+    if (isAlphanum){
+        fs.mkdir(path+folderName);
     } else {
-        return res.status(200).type('application/octet-stream').sendFile(path+fileName);
-    }   
+        throw new Error('Ce nom de dossier n\'est pas accepté !')
+    }
 }
-*/
-
-async function createDirectory(path){
-
-}
-
 
 async function isFolder(path){
     const statsFile = await fs.stat(path);
     return statsFile.isDirectory();
 }
 
+async function deleteFileOrDirectory(path, objName){
+    const regexObjName = new RegExp ("^[a-zA-Z.\-_]+$","gm");
+    const isAlphanum = regexObjName.test(objName.length);
+
+    if (isAlphanum){
+        fs.rm(path+objName, {recursive : true} );
+    } else {
+        throw new Error('Ce nom de dossier n\'est pas accepté !')
+    }
+}
+
+async function uploadFile(){
+    
+}
 
 
-export {listFiles, isFolder, createDirectory}
+export {listDirectory, isFolder, createDirectory, deleteFileOrDirectory, uploadFile}
--- server.js
+++ server.js
@@ -1,15 +1,24 @@
 //const express = require('express');
 import express from 'express';
-import { listFiles, isFolder , createDirectory} from './functions.js'
+import { listDirectory, isFolder, createDirectory, deleteFileOrDirectory, uploadFile} from './functions.js';
+import fileUpload from 'express-fileupload';
+import os from 'os';
 
 const app = express();
 const port = 3000;
 const path = "/home/mariedemeter/Bureau/";
+//const path = os.tmpdir();
 
-app.use(express.static('frontend'))
+
+app.use(express.static('frontend'));
+
+app.use(fileUpload({
+//    useTempFiles : true,
+//    tempFileDir : '/tmp/'
+}));
 
 app.get('/api/drive/', (req, res) => {
-    listFiles(path)
+    listDirectory(path)
         .then(files => res.status(200).type('application/json').send(files))
         .catch(err => console.log(err.message))
 });
@@ -18,22 +27,54 @@ app.get('/api/drive/:name', (req, res) => {
     isFolder(path + req.params.name)
         .then(isFolder => {
             if (isFolder) {
-                listFiles(path + req.params.name + "/")
+                listDirectory(path + req.params.name + "/")
                     .then(nextFiles => res.status(200).type('application/json').send(nextFiles))
                     .catch(err => console.log(err.message))
             } else {
-                console.log('not a directory upload the file');
-                console.log(path + req.params.name);
                 res.status(200).type('application/octet-stream').sendFile(path + req.params.name);
             }
         })
-        .catch (err => res.status(404).send('Erreur 404', err.message));
+        .catch(err => res.status(404).send('Erreur 404' + err.message));
 });
 
-app.post('/api/drive', (req, res) => {
-    createDirectory(path)
+app.post('/api/drive/', (req, res) => {
+    createDirectory(path, req.query.name)
+        .then(newDirectory => res.status(200).send(newDirectory))
+        .catch(err => res.status(400).send('Erreur 400' + err.message));
 });
 
+app.post('/api/drive/:name', (req, res) => {
+    createDirectory(path + req.params.name + '/', req.query.name)
+        .then(newDirectory => res.status(200).send(newDirectory))
+        .catch(err => res.status(400).send('Erreur 400 ' + err.message));
+});
+
+app.delete('/api/drive/:name', (req, res) => {
+    deleteFileOrDirectory(path, req.params.name)
+        .then(objToDelete => res.status(200).send(objToDelete))
+        .catch(err => res.status(400).send('Erreur 400 ' + err.message));
+});
+
+app.delete('/api/drive/:folder/:name', (req, res) => {
+    deleteFileOrDirectory(path+req.params.folder+'/', req.params.name)
+        .then(objToDelete => res.status(200).send(objToDelete))
+        .catch(err => res.status(400).send('Erreur 400 ' + err.message));
+});
+
+app.put('/api/drive/', (req, res) => {
+    console.log(req.files)
+    /*
+    uploadFile(path+req.params.folder+'/', req.params.name)
+        .then(objToDelete => res.status(200).send(objToDelete))
+        .catch(err => res.status(400).send('Erreur 400 ' + err.message));
+        */
+});
+
+app.put('/api/drive/:folder', (req, res) => {
+    uploadFile(path+req.params.folder+'/', req.params.name)
+        .then(objToDelete => res.status(200).send(objToDelete))
+        .catch(err => res.status(400).send('Erreur 400 ' + err.message));
+});
 
 function startServeur() {
     app.listen(port, () => {
